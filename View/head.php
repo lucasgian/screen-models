cc155da305734101f55453e07d5929fa36d511bf
lucasgian <gianextensao@gmail.com>
2017-11-25 14:36:10 -0300

    usando style de idie.software

--- View/head.php
+++ View/head.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
-<html ng-app>
-
+<html lang="en">
 
   <head>
 
@@ -12,18 +11,15 @@
     <title>Gian Fonseca | Skills</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="<?php echo VENDOR.'vendor/bootstrap/css/bootstrap.min.css'; ?>" rel="stylesheet">
+    <link href="<?php echo VENDOR.'vendor/css/bootstrap.min.css'; ?>" rel="stylesheet">
 
     <!-- Custom fonts for this template -->
-    
     <link href="<?php echo VENDOR.'vendor/css/style-page.css'; ?>" rel="stylesheet">
-    <link href="<?php echo VENDOR.'vendor/font-awesome/css/font-awesome.min.css'; ?>" rel="stylesheet">
-    <link href="<?php echo VENDOR.'vendor/devicons/css/devicons.min.css'; ?>" rel="stylesheet">
-    <link href="<?php echo VENDOR.'vendor/simple-line-icons/css/simple-line-icons.css'; ?>" rel="stylesheet">
+    <link href="<?php echo 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'; ?>" rel="stylesheet">
+    <link href="<?php echo VENDOR.'vendor/css/devicons.min.css'; ?>" rel="stylesheet">
+    <link href="<?php echo VENDOR.'vendor/css/simple-line-icons.css'; ?>" rel="stylesheet">
     <link href="<?php echo VENDOR.'vendor/css/devic.min.css'; ?>" rel="stylesheet">
     <!-- Custom styles for this template -->
-    <link href="<?php echo VENDOR.'css/resume.min.css'; ?>" rel="stylesheet">
+    <link href="<?php echo VENDOR.'vendor/css/resume.min.css'; ?>" rel="stylesheet">
 
-
-    <script src="http://code.angularjs.org/1.0.1/angular-1.0.1.min.js"></script>
   </head>
--- config/app.php
+++ config/app.php
@@ -3,5 +3,38 @@
 
 define('VIEW', 'View/');
 define('PAGE', 'Controller/');
-define('VENDOR', 'vendor/');
+define('VENDOR', 'http://idie.software/');
+
+/*
+
+<!DOCTYPE html>
+<html ng-app>
+
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Gian Fonseca | Skills</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="<?php echo VENDOR.'vendor/css/bootstrap.min.css'; ?>" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="<?php echo VENDOR.'vendor/css/style-page.css'; ?>" rel="stylesheet">
+    <link href="<?php echo VENDOR.'vendor/css/font-awesome.min.css'; ?>" rel="stylesheet">
+    <link href="<?php echo VENDOR.'vendor/css/devicons.min.css'; ?>" rel="stylesheet">
+    <link href="<?php echo VENDOR.'vendor/css/simple-line-icons.css'; ?>" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="<?php echo VENDOR.'vendor/css/resume.min.css'; ?>" rel="stylesheet">
+    <link href="<?php echo VENDOR.'vendor/css/devic.min.css'; ?>" rel="stylesheet">
+
+    <script src="http://code.angularjs.org/1.0.1/angular-1.0.1.min.js"></script>
+  </head>
+
+
+*/
 ?>
